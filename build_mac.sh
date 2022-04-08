f68607baecc5ed59d5d80145e79f80a3b926bad5
kaushikkumarbora <kaushikkumarbora@gmail.com>
2022-04-08 15:54:08 +0530

    Commented WebApp docker-compose, Added waiting time between build and start
    Signed-off-by: kaushikkumarbora <kaushikkumarbora@gmail.com>

--- build_mac.sh
+++ build_mac.sh
@@ -3,4 +3,5 @@ cp ./binary_mac/* .
 export FABRIC_CFG_PATH=$PWD
 sh ./generate-certs-mac.sh
 sh ./docker-images.sh
+sleep 5
 docker-compose up -d
--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -3,4 +3,5 @@ cp ./binary_ubuntu/* .
 export FABRIC_CFG_PATH=$PWD
 sh ./generate-certs-ubuntu.sh
 sh ./docker-images.sh
+sleep 5
 docker-compose up -d
