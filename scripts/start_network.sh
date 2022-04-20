c5c70e4092a595f6942e524c5fb94b6740123fb7
kaushikkumarbora <kaushikkumarbora@gmail.com>
2022-04-21 01:21:42 +0530

    Remove old cc images
    Signed-off-by: kaushikkumarbora <kaushikkumarbora@gmail.com>

--- scripts/start_network.sh
+++ scripts/start_network.sh
@@ -3,4 +3,5 @@ docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker system prune
 docker volume rm $(docker volume ls -q)
+docker rmi $(docker images -f reference=dev-* -q)
 docker-compose -f docker-compose.yaml up
