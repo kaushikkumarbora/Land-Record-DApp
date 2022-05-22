7afbd223a86fc5d56874f8717609ff757410b511
kaushikkumarbora <kaushikkumarbora@gmail.com>
2022-05-22 20:30:22 +0530

    Implemented Municipal
    Signed-off-by: kaushikkumarbora <kaushikkumarbora@gmail.com>

--- client/decLand/src/Municipal/Municipal.jsx
+++ client/decLand/src/Municipal/Municipal.jsx
@@ -1,9 +1,101 @@
 import styles from '../App.module.css'
+import {
+  getRegistrations,
+  Registration
+} from '../dataRegistration/Registration'
+import { onMount, createSignal, Show } from 'solid-js'
+import { Alert, Spinner } from 'solid-bootstrap'
+import { SearchField } from './Search'
+import { FullRegistration } from '../dataRegistration/FullRegistration'
+import { FullDnC } from '../dataRegistration/FullDnC'
+import { FullCovenants } from '../dataRegistration/FullCovenants'
+import './municipal.css'
+import { ProcessMunicipal } from './ProcessMunicipal'
 
 export function Municipal () {
+  const [registrations, setRegistrations] = createSignal([])
+  const [selectedRegistration, setSelectedRegistration] = createSignal({})
+  const [searching, setSearching] = createSignal(false)
+
+  const searchRegistrations = async params => {
+    setSearching(true)
+    let res = getRegistrations('/municipal/api/registrations', params)
+    res.then(data => {
+      // data = []
+      // data.push({
+      //   CustID: '123',
+      //   RealEstateID: 'asdasd',
+      //   LoanAmount: Math.random(),
+      //   Fico: Math.random(),
+      //   Status: 'OK',
+      //   MortgageStatus: 'OK'
+      // })
+      setSearching(false)
+      setRegistrations(data)
+    })
+  }
+
+  onMount(() => searchRegistrations({ permission: false }))
+
+  console.log('Municipal Rendered')
   return (
-    <>
-      <div class={styles.other}></div>
-    </>
+    <div class='container-fluid'>
+      <div class='row'>
+        <div
+          class={
+            'col-sm-3 col-md-6 col-lg-4 ' + styles.other + ' ' + styles.splits
+          }
+        >
+          <br />
+          <SearchField getRegistrations={searchRegistrations} />
+          <Alert variant='secondary'>
+            {console.log(searching())}
+            <Show
+              when={!searching()}
+              fallback={
+                <div class={styles.bodyfont}>
+                  <Spinner animation='border' role='status'>
+                    <span class='visually-hidden'>Loading...</span>
+                  </Spinner>
+                </div>
+              }
+            >
+              <Show
+                when={registrations().length != 0}
+                fallback={<div class={styles.bodyfont}>No Entries</div>}
+              >
+                <Registration
+                  data={registrations()}
+                  onClick={setSelectedRegistration}
+                />
+              </Show>
+            </Show>
+          </Alert>
+        </div>
+        <div class={'col-sm-9 col-md-6 col-lg-8 ' + styles.splits}>
+          <br />
+          <div class='municipalwin'>
+            <div class='registration'>
+              <FullRegistration item={selectedRegistration()} />
+            </div>
+            <div class='dnc'>
+              <FullDnC item={selectedRegistration().DnC} />
+            </div>
+            <div class='covenants'>
+              <FullCovenants item={selectedRegistration().Covenants} />
+            </div>
+            <div class='municipal flex-container'>
+              <Alert variant='light'>
+                <Alert.Heading>Actions</Alert.Heading>
+                <ProcessMunicipal
+                  class='flex-items'
+                  item={selectedRegistration()}
+                />
+              </Alert>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ client/decLand/src/Municipal/ProcessMunicipal.jsx
@@ -0,0 +1,107 @@
+import {
+  Alert,
+  Placeholder,
+  Form,
+  Row,
+  Button,
+  Col,
+  Spinner,
+  Modal
+} from 'solid-bootstrap'
+import { createSignal } from 'solid-js'
+
+export function ProcessMunicipal (props) {
+  const [processing, setProcessing] = createSignal(false)
+  const [showSmS, setShowS] = createSignal(false)
+  const [showSmF, setShowF] = createSignal(false)
+  const handleCloseSmS = () => setShowS(false)
+  const handleCloseSmF = () => setShowF(false)
+
+  async function permit (RealEstateID) {
+    setProcessing(true)
+    fetch('/municipal/api/set-permission', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        RealEstateID
+      })
+    }).then(response => {
+      if (response.status === 200) {
+        setShowS(true)
+        setProcessing(false)
+      } else {
+        setShowF(true)
+        setProcessing(false)
+      }
+    })
+  }
+
+  return (
+    <>
+      <Modal
+        size='sm'
+        show={showSmS()}
+        onHide={handleCloseSmS}
+        aria-labelledby='example-modal-sizes-title-sm'
+      >
+        <Modal.Body>Success</Modal.Body>
+      </Modal>
+      <Modal
+        size='sm'
+        show={showSmF()}
+        onHide={handleCloseSmF}
+        aria-labelledby='example-modal-sizes-title-sm'
+      >
+        <Modal.Body>Failed</Modal.Body>
+      </Modal>
+      <Alert variant='success'>
+        <Form>
+          <Row class='mb-3'>
+            <Form.Group as={Col}>
+              <Show
+                when={typeof props.item.RealEstateID != 'undefined'}
+                fallback={
+                  <Placeholder as={Form.Label} animation='wave'>
+                    RealEstateID:{' '}
+                    <code>
+                      <Placeholder xs={3} />
+                    </code>
+                  </Placeholder>
+                }
+              >
+                <Form.Label>
+                  RealEstateID: <code>{props.item.RealEstateID}</code>
+                </Form.Label>
+              </Show>
+            </Form.Group>
+          </Row>
+          <Show
+            when={processing}
+            fallback={
+              <Button variant='primary' disabled>
+                <Spinner
+                  as='span'
+                  animation='border'
+                  size='sm'
+                  role='status'
+                  aria-hidden='true'
+                />
+                <span class='visually-hidden'>Loading...</span>
+              </Button>
+            }
+          >
+            <Button
+              variant='primary'
+              onClick={event => {
+                permit(props.item.RealEstateID)
+              }}
+              disabled={typeof props.item.RealEstateID === 'undefined'}
+            >
+              Permit
+            </Button>
+          </Show>
+        </Form>
+      </Alert>
+    </>
+  )
+}
