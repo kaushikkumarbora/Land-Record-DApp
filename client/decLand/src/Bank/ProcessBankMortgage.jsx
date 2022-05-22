2ea267296002b57a257d2dd7833b2d1bc97dea12
kaushikkumarbora <kaushikkumarbora@gmail.com>
2022-05-22 13:42:25 +0530

    Added Actions to Bank
    Signed-off-by: kaushikkumarbora <kaushikkumarbora@gmail.com>

--- client/decLand/src/Bank/ProcessBankMortgage.jsx
+++ client/decLand/src/Bank/ProcessBankMortgage.jsx
@@ -6,24 +6,23 @@ import {
   Button,
   Col,
   Spinner,
-  Modal,
-  InputGroup
+  Modal
 } from 'solid-bootstrap'
 import { createSignal } from 'solid-js'
 
-export function ProcessBank (props) {
+export function ProcessBankMortgage (props) {
   const [processing, setProcessing] = createSignal(false)
   const [showSmS, setShowS] = createSignal(false)
   const [showSmF, setShowF] = createSignal(false)
   const handleCloseSmS = () => setShowS(false)
   const handleCloseSmF = () => setShowF(false)
 
-  async function initiateLoan (CustID, RealEstateID, LoanAmount) {
+  async function initiateMortgage (CustID, RealEstateID) {
     setProcessing(true)
-    fetch('/bank/api/initiate-loan', {
+    fetch('/bank/api/initiate-mortgage', {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ CustID, RealEstateID, LoanAmount })
+      body: JSON.stringify({ CustID, RealEstateID })
     }).then(response => {
       if (response.status === 200) {
         setShowS(true)
@@ -35,12 +34,12 @@ export function ProcessBank (props) {
     })
   }
 
-  async function processLoan (CustID, RealEstateID, Amount) {
+  async function closeMortgage (CustID, RealEstateID) {
     setProcessing(true)
-    fetch('/bank/api/process-loan', {
+    fetch('/bank/api/close-mortgage', {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ CustID, RealEstateID, Amount })
+      body: JSON.stringify({ CustID, RealEstateID })
     }).then(response => {
       if (response.status === 200) {
         setShowS(true)
@@ -71,56 +70,8 @@ export function ProcessBank (props) {
       >
         <Modal.Body>Failed</Modal.Body>
       </Modal>
-      <Alert variant='success'>
-        <Alert.Heading>Initiate Loan</Alert.Heading>
-        <Form>
-          <Row class='mb-3'>
-            <Form.Group as={Col}>
-              <Form.Label>CustID:</Form.Label>
-              <Form.Control type='text' placeholder='Enter Aadhar Number' />
-              <br />
-              <Form.Label>RealEstateID:</Form.Label>
-              <Form.Control type='text' placeholder='Enter RealEstate ID' />
-              <br />
-              <Form.Label>Amount</Form.Label>
-              <InputGroup>
-                <InputGroup.Text>Rs.</InputGroup.Text>
-                <Form.Control type='number' placeholder='Enter Amount in Rs.' />
-              </InputGroup>
-            </Form.Group>
-          </Row>
-          <Show
-            when={processing}
-            fallback={
-              <Button variant='primary' disabled>
-                <Spinner
-                  as='span'
-                  animation='border'
-                  size='sm'
-                  role='status'
-                  aria-hidden='true'
-                />
-                <span class='visually-hidden'>Loading...</span>
-              </Button>
-            }
-          >
-            <Button
-              variant='primary'
-              onClick={event => {
-                initiateLoan(
-                  event.target.form[0].value,
-                  event.target.form[1].value,
-                  event.target.form[2].value
-                )
-              }}
-            >
-              Initiate
-            </Button>
-          </Show>
-        </Form>
-      </Alert>
-      <Alert variant='success'>
-        <Alert.Heading>Process Loan</Alert.Heading>
+      <Alert variant='warning'>
+        <Alert.Heading>Initiate Mortgage</Alert.Heading>
         <Form>
           <Row class='mb-3'>
             <Form.Group as={Col}>
@@ -147,12 +98,6 @@ export function ProcessBank (props) {
                   RealEstateID: <code>{props.item.RealEstateID}</code>
                 </Form.Label>
               </Show>
-              <br />
-              <Form.Label>Action</Form.Label>
-              <Form.Select disabled={typeof props.item.CustID === 'undefined'}>
-                <option value={true}>Approve</option>
-                <option value={false}>Reject</option>
-              </Form.Select>
             </Form.Group>
           </Row>
           <Show
@@ -173,15 +118,68 @@ export function ProcessBank (props) {
             <Button
               variant='primary'
               onClick={event => {
-                processLoan(
-                  props.item.CustID,
-                  props.item.RealEstateID,
-                  event.target.form[0].value
-                )
+                initiateMortgage(props.item.CustID, props.item.RealEstateID)
               }}
-              disabled={props.item.Status != 'Applied'}
+              disabled={props.item.Status != 'Approved'}
             >
-              Process
+              Initiate
+            </Button>
+          </Show>
+        </Form>
+      </Alert>
+      <Alert variant='warning'>
+        <Alert.Heading>Close Mortgage</Alert.Heading>
+        <Form>
+          <Row class='mb-3'>
+            <Form.Group as={Col}>
+              <Show
+                when={typeof props.item.CustID != 'undefined'}
+                fallback={
+                  <Placeholder as={Form.Label} animation='wave'>
+                    CustID:{' '}
+                    <code>
+                      <Placeholder xs={4} />
+                    </code>
+                    RealEstateID:{' '}
+                    <code>
+                      <Placeholder xs={3} />
+                    </code>
+                  </Placeholder>
+                }
+              >
+                <Form.Label>
+                  CustID: <code>{props.item.CustID}</code>
+                </Form.Label>
+                <br />
+                <Form.Label>
+                  RealEstateID: <code>{props.item.RealEstateID}</code>
+                </Form.Label>
+              </Show>
+            </Form.Group>
+          </Row>
+          <Show
+            when={processing}
+            fallback={
+              <Button variant='primary' disabled>
+                <Spinner
+                  as='span'
+                  animation='border'
+                  size='sm'
+                  role='status'
+                  aria-hidden='true'
+                />
+                <span class='visually-hidden'>Loading...</span>
+              </Button>
+            }
+          >
+            <Button
+              variant='primary'
+              onClick={event => {
+                closeMortgage(props.item.CustID, props.item.RealEstateID)
+              }}
+              disabled={props.item.Status != 'Inforced'}
+            >
+              Close
             </Button>
           </Show>
         </Form>
